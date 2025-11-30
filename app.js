ba50b9c48eebd4dff45b3c80d234b62acfa66fa3
JonathanPothin <79927431+JonathanPothin@users.noreply.github.com>
2025-11-30 14:26:14 +0100

    Update app.js

--- app.js
+++ app.js
@@ -219,7 +219,7 @@ function renderEventCard(ev, participantsByEvent) {
 
       <div class="join-card">
         <input type="text" id="name-${ev.id}" placeholder="Nom">
-        <p class="contact-hint"> Choisissez un moyen de contact : E-mail ou SMS. </p>
+        <p class="contact-hint"> Choisissez un moyen de contact: Mail ou SMS</p>
         <input type="email" id="email-${ev.id}" placeholder="Saisissez votre Email">
         <input type="tel" id="phone-${ev.id}" placeholder="Saisissez votre Téléphone">
         <button class="btn btn-full" onclick="joinEvent('${ev.id}')">Je participe</button>
